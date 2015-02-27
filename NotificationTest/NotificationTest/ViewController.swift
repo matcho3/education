71d27f70839d4ef4df46e2a82080b6a50052edf5
matcho3 <matcho3tk@gmail.com>
2015-02-27 14:49:27 +0900

    first commit

---
+++ NotificationTest/NotificationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationTest
+//
+//  Created by KOHAMA TAKUYA on 2/26/15.
+//  Copyright (c) 2015 KOHAMA TAKUYA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
